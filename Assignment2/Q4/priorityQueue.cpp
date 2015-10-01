5f822454fb62a960a66504c2bd43d85ec24ea837
Brayden Streibel <Vince@Tali.accesscomm.ca>
2015-09-30 20:45:07 -0600

    Added comments

--- Assignment2/Q4/priorityQueue.cpp
+++ Assignment2/Q4/priorityQueue.cpp
@@ -24,19 +24,22 @@ void PQ::BuildHeap()
     int i = j;
     while (i > 0)
     {
-        Job temp = a[i];
-        while (i * 2 <= elementCount)
+        Job temp = a[i]; // Copy parent
+        while (i * 2 <= elementCount) // Loop through each child
         {
             int child = i * 2;
+            // Compare children, check if right child is smaller than left child
             if (child != elementCount && (a[child + 1].priority < a[child].priority ||
                 (a[child + 1].priority == a[child].priority && a[child + 1].fileSize < a[child].fileSize)))
             {
                 child++;
             }
+            // Compare parent and child priority
             if (a[child].priority < temp.priority)
             {
                 a[i] = a[child];
             }
+            // If priority the same, compare parent and child filesize.
             else if (a[child].priority == temp.priority && a[child].fileSize < temp.fileSize)
             {
                 a[i] = a[child];
@@ -44,7 +47,7 @@ void PQ::BuildHeap()
             else break;
             i = child;
         }
-        a[i] = temp;
+        a[i] = temp; // Copy parent to location it satisfies heap order property
         j--;
         i = j;
     }
@@ -54,12 +57,16 @@ void PQ::BuildHeap()
 void PQ::List()
 {
     int originalCount = elementCount;
+    
+    // Delete min and place value back into array without incrementing element size
 	while (elementCount > 0)
 	{
         Job temp = ReleaseNext();
         temp.Print();
         a[elementCount+1] = temp;
 	}
+    
+    // Rebuild heap using original count and array values
     elementCount = originalCount;
     BuildHeap();
 }
@@ -69,6 +76,8 @@ void PQ::Add(Job toAdd)
 	toAdd.jobNo = jobID++;
 	elementCount++;
 	int i = elementCount;
+    
+    // Check if parent is lower priority, swap if so
 	while (i > 1 && (toAdd.priority < a[i / 2].priority ||
                     (toAdd.priority == a[i / 2].priority && toAdd.fileSize < a[i / 2].fileSize)))
 	{
@@ -80,6 +89,7 @@ void PQ::Add(Job toAdd)
 
 void PQ::Remove(int jobID)
 {
+    // Find job to delete, swap it and last value and decrement count
     for (int i = 1; i < elementCount; i++)
     {
         if (a[i].jobNo == jobID)
@@ -89,21 +99,25 @@ void PQ::Remove(int jobID)
             break;
         }
     }
+    // Rebuild
     BuildHeap();
 }
 
 Job PQ::ReleaseNext()
 {
+    // Swap root and last nodes, decrement count
     Job toRet = a[1];
     a[1] = a[elementCount];
     elementCount--;
     
+    // Rebuild
     BuildHeap();
     return toRet;
 }
 
 void PQ::ChangePriority(int jobID, int newPriority)
 {
+    // Find job to edit, change priority
     for (int i = 1; i < elementCount; i++)
     {
         if (a[i].jobNo == jobID)
@@ -112,5 +126,6 @@ void PQ::ChangePriority(int jobID, int newPriority)
             break;
         }
     }
+    // Rebuild
     BuildHeap();
 }
