7040cb99feb5b0b5eba1142e5851de0451518041
Nickknack <Nickknack17@gmail.com>
2015-11-19 15:23:02 -0600

    Assignment 5 question 1 is completed and working.

--- Assignment5/main.cpp
+++ Assignment5/main.cpp
@@ -1,7 +1,7 @@
 //***************************************************************************
 //Names: Nicholas Kowalchuk, Brayden Streibal, Evan Boyle
 //Created: November 12th, 2015
-//Last Modified: November 12th, 2015
+//Last Modified: November 19th, 2015
 //Purpose: Run a string of characters through the 4 string pattern matching
 //		   Algorithms and count the amount of comparisons. 
 //***************************************************************************
@@ -13,28 +13,34 @@
 
 using namespace std;
 
-const int MAX_ARRAY = 500;
-const int PATTERN_SIZE = 4;
+const int MAX_ARRAY = 9000;
 
 int main ()
 {
 	//Variable Declerations
 	char string[MAX_ARRAY];
 	int matches[MAX_ARRAY];
-	char pattern[PATTERN_SIZE] {'a', 'b', 'c', 'd'};
+	char pattern[MAX_ARRAY];
 	char bufferChar;
-	ifstream input;
-	int stringSize;
+	ifstream input, inPattern;
+	int stringSize, patternSize;
 	int matchCount = 0;
 	int comps = 0;
 
 
-	//opening and testing the file
-	input.open("input.txt");
+	//opening and testing the files
+	input.open("text.txt");
+	inPattern.open("patterns.txt");
 
 	if (!input)
 	{
-		cout << endl << endl << "ERROR: Unable to open file." << endl << endl;
+		cout << endl << endl << "ERROR: Unable to open input.txt" << endl << endl;
+		return 1;
+	}
+
+	if (!inPattern)
+	{
+		cout << endl << endl << "ERROR: Unable to open pattern.txt" << endl << endl;
 		return 1;
 	}
 
@@ -44,32 +50,135 @@ int main ()
 	//is reached or the maximum size of the array is reached. 
 	input.get(bufferChar);
 
+	//read in the string from the file.
 	while (!(input.eof()) && (stringSize < MAX_ARRAY))
 	{
-		//If the character read in isn't a whitespace, put it in the array
-		if (bufferChar != ' ')
-		{
-			string[stringSize++] = bufferChar;
-		}
+		string[stringSize++] = bufferChar;
 		input.get(bufferChar);
 	}
 
-	comps = BruteForce(string, pattern, matches, stringSize, PATTERN_SIZE, matchCount);
-
-	cout << endl << "*****BRUTE FORCE****" << endl << endl;
-	cout << "There are " << matchCount << " matches." << endl;
-
-	if (matchCount > 0)
+	//Each line of the pattern file is a new patter for matching with the string. 
+	//While we haven't reached the end of pattern file we are going to read in 1 full line
+	//from the pattern file (until a newline character is reached) and then we will run the
+	//pattern matching algorithms on the line. We continue reading in lines from the file and then
+	//running the algorithms on the line until we have reached the end of the file. 
+	inPattern.get(bufferChar);
+	while (!(inPattern.eof()))
 	{
-		cout << "matches at:" << endl;
-	}
+		patternSize = 0;
 
-	for (int i = 0; i < matchCount; i++)
-	{
-		cout << "    position " << matches[i] << endl;
-	}
+		while ((bufferChar != '\n') && (patternSize < MAX_ARRAY))
+		{
+			pattern[patternSize++] = bufferChar;
+			inPattern.get(bufferChar);
+		}
 
-	cout << comps << " comparisons where made." << endl << endl;
+		cout << endl << "Pattern matching on: ";
+
+		for (int i = 0; i < patternSize; i++)
+		{
+			cout << pattern[i];
+		}
+
+		//***************START: BRUTE FORCE***************
+
+		//Use the BruteForce algorithm for pattern matching.
+		comps = BruteForce(string, pattern, matches, stringSize, patternSize, matchCount);
+
+		//display the results.
+		cout << endl << "*****BRUTE FORCE****" << endl << endl;
+		cout << "There are " << matchCount << " matches." << endl;
+
+		if (matchCount > 0)
+		{
+			cout << "matches at:" << endl;
+		}
+
+		for (int i = 0; i < matchCount; i++)
+		{
+			cout << "    position " << matches[i] << endl;
+		}
+
+		cout << comps << " comparisons where made." << endl << endl;
+
+		//***************END: BRUTE FORCE***************
+
+		//reset matchCount to 0 for the next patternMatching algorithm.
+		matchCount = 0;
+
+		//***************START: BOYER-MOORE***************
+
+		//Use the Boyer-Moore algorithm for pattern matching
+		comps = BoyerMoore(string, pattern, matches, stringSize, patternSize, matchCount, true);
+
+		//display the results.
+		cout << endl << "*****BOYER-MOORE****" << endl << endl;
+		cout << "There are " << matchCount << " matches." << endl;
+
+		if (matchCount > 0)
+		{
+			cout << "matches at:" << endl;
+		}
+
+		for (int i = 0; i < matchCount; i++)
+		{
+			cout << "    position " << matches[i] << endl;
+		}
+
+		cout << comps << " comparisons where made." << endl << endl;
+
+		//***************END: BOYER-MOORE***************
+
+		//reset matchCount to 0 for the next patternMatching algorithm.
+		matchCount = 0;
+
+		//***************START: BOYER-MOORE(without the character jump heuristic)***************
+		comps = BoyerMoore(string, pattern, matches, stringSize, patternSize, matchCount, false);
+
+		//display the results.
+		cout << endl << "*****BOYER-MOORE (without Character Jump Heuristic)****" << endl << endl;
+		cout << "There are " << matchCount << " matches." << endl;
+
+		if (matchCount > 0)
+		{
+			cout << "matches at:" << endl;
+		}
+
+		for (int i = 0; i < matchCount; i++)
+		{
+			cout << "    position " << matches[i] << endl;
+		}
+
+		cout << comps << " comparisons where made." << endl << endl;
+
+		//***************END: BOYER-MOORE(without the character jump heuristic)***************
+		matchCount = 0;
+
+		//***************START: KNUTH-MORRIS-PRATT***************
+		comps = KnuthMorrisPratt(string, pattern, matches, stringSize, patternSize, matchCount);
+
+		//display the results.
+		cout << endl << "*****KNUTH-MORRIS-PRATT****" << endl << endl;
+		cout << "There are " << matchCount << " matches." << endl;
+
+		if (matchCount > 0)
+		{
+			cout << "matches at:" << endl;
+		}
+
+		for (int i = 0; i < matchCount; i++)
+		{
+			cout << "    position " << matches[i] << endl;
+		}
+
+		cout << comps << " comparisons where made." << endl << endl;
+
+		//***************END: KNUTH-MORRIS-PRATT***************
+
+		matchCount = 0;
+
+		inPattern.get(bufferChar);
+	}
 
 	return 0;
 }
--- Assignment5/match.h
+++ Assignment5/match.h
@@ -1,7 +1,7 @@
 //***************************************************************************
 //Names: Nicholas Kowalchuk, Brayden Streibal, Evan Boyle
 //Created: November 12th, 2015
-//Last Modified: November 12th, 2015
+//Last Modified: November 19th, 2015
 //Purpose: pattern matching algorithms
 //***************************************************************************
 
@@ -25,5 +25,52 @@ int BruteForce(char string[], char pattern[], int matches[], int stringSize, int
 //Return: returns the amount of comparisions made for the algorithm.
 //Side Effect: the matches array will now contain the indexes of all the matches and matchCount will
 //			   the amount of matches made. 
+
+int BoyerMoore (char string[], char pattern[], int matches[], int stringSize, int patternSize, int& matchCount, bool jumpHeur);
+//purpose: Finds all of the matching patterns in the string of characters.
+//Parameter(s): an array of chars called string which is the string we are searching through
+//				an array of chars called pattern which is the pattern we are trying to find in string
+//				an array of ints called matches; this contains the indexes for all the matches
+//				an int called stringSize indicating the size of the string array
+//				an int called patternSize indicating the size of the pattern array
+//				an int called matchCount which counts the amount of matches that are made. 
+//				a bool valued called jumpHeur which determines wether we are going to use the character
+//					jump heuristic or not. 
+//Precondition(s): string and pattern have been initialized. 
+//Return: returns the amount of comparisions made for the algorithm.
+//Side Effect: the matches array will now contain the indexes of all the matches and matchCount will
+//			   the amount of matches made. 
+
+int Last (char character, char pattern[], int patternSize, int& compCount);
+//purpose: Finds the first occurence of a character in an array of characters working from the last
+//		   element to the 0th element.
+//Parameter(s): a char called character which is the character we are searching the array for.
+//				an array of chars called pattern which is what we are searching.
+//              an int called patternSize which is the amount of elements in the array. 
+//				an int called compCount which tracks the amount of comparisons made. 
+//Return: returns an integer which is the index of the first match of the character. if no match
+//		  then a -1 is returned. 
+//Side Effect: compCount will be incremented for each comparison made in the algorithm.
+
+int KnuthMorrisPratt (char string[], char pattern[], int matches[], int stringSize, int patternSize, int& matchCount);
+//purpose: Finds all of the matching patterns in the string of characters.
+//Parameter(s): an array of chars called string which is the string we are searching through
+//				an array of chars called pattern which is the pattern we are trying to find in string
+//				an array of ints called matches; this contains the indexes for all the matches
+//				an int called stringSize indicating the size of the string array
+//				an int called patternSize indicating the size of the pattern array
+//				an int called matchCount which counts the amount of matches that are made. 
+//Precondition(s): string and pattern have been initialized. 
+//Return: returns the amount of comparisions made for the algorithm.
+//Side Effect: the matches array will now contain the indexes of all the matches and matchCount will
+//			   the amount of matches made. 
+
+void Failure(int f[], char pattern[], int patternSize);
+//purpose: sets up a failure array (f) that keeps track of substrings within the pattern string. 
+//Parameter(s): an array of ints called f. this is the failure array. 
+//				an array of chars called pattern which is what we are finding substrings in.
+//              an int called patternSize which is the amount of elements in the pattern array. 
+//Return: N/A
+//Side Effect: the f array will now contain values associated with the substrings found. 
 
 #endif
